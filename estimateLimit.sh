5bbca8000f5edfa7e6f26d348f683b70c32b22ba
Pietro Vischia <pietro.vischia@gmail.com>
2016-12-16 10:01:35 +0100

    Added full CLs run, workspace conversion, and signal syst variation

--- estimateLimit.sh
+++ estimateLimit.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 BASELOCATION="baseCards/"
-BASEDATACARD="datacard_DeltaPhi_ElMu_0.txt"
+BASEDATACARD="datacard_DeltaPhi_ElMu_0"
 BASESHAPESFILE="DeltaPhi_ElMu_0.root"
 VARIEDLOCATION="variedTtbar"
 
 # RUN [Asy|Ful]
-RUN="Asy"
+# RUN="Asy"
 # BANDS [bool]
 BANDS=false
 
 ### This function runs limits for a single datacard
 function runLimit {
     DATACARD=$1
+    RUN=$2
     if   [ "$RUN" == "Asy" ]; then
         combine -M Asymptotic ${DATACARD} -n ${DATACARD}
     elif [ "$RUN" == "Ful" ]; then
@@ -29,25 +30,38 @@ function runLimit {
 
 ### This function creates syst cards with varied systematics ###
 function createSetSysts {
-    BASE=$1
-    SHAPE=$2
-    VARIED=$3
+    WHAT=$1
+    BASE=$2
+    SHAPE=$3
+    VARIED=$4
     mkdir ${VARIED}
     cp ${BASELOCATION}/${SHAPE} ${VARIED}
-    for ttbarSys in 01 05 10 20 25 30 40 50 60 70 80 90 99; do
-        cp ${BASELOCATION}/${BASE} ${VARIED}/varied_${ttbarSys}_${BASE}
-        sed -i -- "s/ttbar                 lnN     -       -       -       -        1.25      - /ttbar                 lnN     -       -       -       -        1.$ttbarSys      - /g" ${VARIED}/varied_${ttbarSys}_${BASE}
-    done
+    if [ "$WHAT" == "ttbar" ]; then
+        for ttbarSys in 01 05 10 20 25 30 40 50 60 70 80 90 99; do
+            cp ${BASELOCATION}/${BASE}.txt ${VARIED}/varied_${ttbarSys}_${BASE}.txt
+            sed -i -- "s/ttbar                 lnN     -       -       -       -        1.25      - /ttbar                 lnN     -       -       -       -        1.$ttbarSys      - /g" ${VARIED}/varied_${ttbarSys}_${BASE}.txt
+            text2workspace.py ${VARIED}/varied_${ttbarSys}_${BASE}.txt -o ${VARIED}/varied_${ttbarSys}_${BASE}.root
+        done
+    elif [ "$WHAT" == "signal" ]; then
+        for signalSys in 01 10 20 50 99; do
+            cp ${BASELOCATION}/${BASE}.txt ${VARIED}/varied_${signalSys}_${BASE}.txt
+            sed -i -- "s/Signal                lnN     -       -       -       -        -         1.20 /Signal                lnN     -       -       -       -        -         1.$signalSys /g" ${VARIED}/varied_${signalSys}_${BASE}.txt
+            text2workspace.py ${VARIED}/varied_${signalSys}_${BASE}.txt -o ${VARIED}/varied_${signalSys}_${BASE}.root
+        done
+    fi
 
 }
 ######
+
+
 
 ### This function runs limits for the cards created via the createSetSysts function ###
 function runSetSysts {
     LOCATION=$1
     cd ${LOCATION}
-    for CARD in `ls ./`; do
-        runLimit ${CARD}
+    for CARD in `ls varied*root `; do
+        runLimit ${CARD} Asy
+        runLimit ${CARD} Ful
     done
     cd -
 }
@@ -63,10 +77,12 @@ function analyzeVariedSysts {
 ###### MAIN ######
 
 # Create set of varied cards for base shape card
-#createSetSysts ${BASEDATACARD} ${BASESHAPESFILE} ${VARIEDLOCATION}
+createSetSysts ttbar  ${BASEDATACARD} ${BASESHAPESFILE} ${VARIEDLOCATION}
+createSetSysts signal ${BASEDATACARD} ${BASESHAPESFILE} ${VARIEDLOCATION}_signal
 
 # Run limits on the cards with varied systs
-#runSetSysts ${VARIEDLOCATION}
+runSetSysts ${VARIEDLOCATION}
+runSetSysts ${VARIEDLOCATION}_signal
 
 # Analyze the outcome
 analyzeVariedSysts ${VARIEDLOCATION}
