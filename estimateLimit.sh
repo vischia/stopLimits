10eee986c317a47b7b1e75dc6e4507695e49e274
Pietro Vischia <pietro.vischia@gmail.com>
2017-06-13 18:52:03 +0200

    N-th change of format

--- estimateLimit.sh
+++ estimateLimit.sh
@@ -98,7 +98,7 @@ function analyzeVariedSysts {
     if [ "$SITUATIONONLY" == false ]; then
         eval "root -l analyzeVariedSysts.C\\(\\\"${WHAT}\\\",\\\"${LOCATION}/\\\",\\\"${BASEDATACARD}\\\",\\\"${RUNFULL}\\\"\\)"
     fi
-    eval "root -l drawSituation.C\\(\\\"${WHO}\\\",\\\"${BASELOCATION}${BASESHAPESFILE}\\\",\\\"${LOCATION}\\\",\\\"${THELABEL}\\\"\\)"
+    eval "root -l -b drawSituation.C\\(\\\"${WHO}\\\",\\\"${BASELOCATION}${BASESHAPESFILE}\\\",\\\"${LOCATION}\\\",\\\"${THELABEL}\\\",\\\"${SIGNALLABEL}\\\"\\)"
 
 }
 ######
@@ -110,6 +110,7 @@ function main {
     THELABEL=$3
     BASESHAPESFILE=$4
     VARIEDLOCATION=$5
+    SIGNALLABEL=$6
 
     if [ "$ALL" == true ]; then
         # Create set of varied cards for base shape card
@@ -142,8 +143,96 @@ function main {
 #main "2017-03-15_juan/" "datacard_TMT2_ElMu_S-200-50" "M_{T2}" "TMT2_ElMu.root" "varied_2017-03-15_juan_MT2"
 
 
-main "2017-03-22_juan/" "datacard_CutAndCount_ElMu_S_242_75" "CutNCount" "CutAndCount_ElMu.root" "varied_2017-03-22_juan_CnC"
-main "2017-03-22_juan/" "datacard_TMT2_ElMu_S_242_75" "M_{T2}" "TMT2_ElMu.root" "varied_2017-03-22_juan_MT2"
+#main "2017-03-22_juan/" "datacard_CutAndCount_ElMu_S_242_75" "CutNCount" "CutAndCount_ElMu.root" "varied_2017-03-22_juan_CnC"
+#main "2017-03-22_juan/" "datacard_TMT2_ElMu_S_242_75" "M_{T2}" "TMT2_ElMu.root" "varied_2017-03-22_juan_MT2"
+
+#mkdir -p ~/www/stop/old/
+#rm -r  ~/www/stop/old/varied_2017-03-22_juan_CnC/
+#rm -r  ~/www/stop/old/varied_2017-03-22_juan_CnC_signal/
+#rm -r  ~/www/stop/old/varied_2017-03-22_juan_MT2/
+#rm -r  ~/www/stop/old/varied_2017-03-22_juan_MT2_signal/
+#
+#mv ~/www/stop/varied_2017-03-22_juan_CnC/        ~/www/stop/old/  
+#mv ~/www/stop/varied_2017-03-22_juan_CnC_signal/ ~/www/stop/old/
+#mv ~/www/stop/varied_2017-03-22_juan_MT2/        ~/www/stop/old/
+#mv ~/www/stop/varied_2017-03-22_juan_MT2_signal/ ~/www/stop/old/ 
+#
+#cp -r varied_2017-03-22_juan_CnC/        ~/www/stop/
+#cp -r varied_2017-03-22_juan_CnC_signal/ ~/www/stop/
+#cp -r varied_2017-03-22_juan_MT2/        ~/www/stop/
+#cp -r varied_2017-03-22_juan_MT2_signal/ ~/www/stop/
+#
+#cp ~/www/stop/index.php ~/www/stop/varied_2017-03-22_juan_CnC/
+#cp ~/www/stop/index.php ~/www/stop/varied_2017-03-22_juan_CnC_signal/
+#cp ~/www/stop/index.php ~/www/stop/varied_2017-03-22_juan_MT2/
+#cp ~/www/stop/index.php ~/www/stop/varied_2017-03-22_juan_MT2_signal/
+#
+
+main "2017-06-13_juan/" "datacard_MT2_ElMu_SFS_200_50" "M_{T2}" "MT2.root" "varied_2017-06-13_juan_MT2_200" "SFS_200_50"
+main "2017-06-13_juan/" "datacard_MT2_ElMu_SFS_225_50" "M_{T2}" "MT2.root" "varied_2017-06-13_juan_MT2_225" "SFS_225_50"
+main "2017-06-13_juan/" "datacard_MT2_ElMu_SFS_250_50" "M_{T2}" "MT2.root" "varied_2017-06-13_juan_MT2_250" "SFS_250_50"
+
+main "2017-06-13_juan/" "datacard_CutAndCount_ElMu_SFS_200_50" "Yield" "CutAndCount.root" "varied_2017-06-13_juan_CutAndCount_200" "SFS_200_50"
+main "2017-06-13_juan/" "datacard_CutAndCount_ElMu_SFS_225_50" "Yield" "CutAndCount.root" "varied_2017-06-13_juan_CutAndCount_225" "SFS_225_50"
+main "2017-06-13_juan/" "datacard_CutAndCount_ElMu_SFS_250_50" "Yield" "CutAndCount.root" "varied_2017-06-13_juan_CutAndCount_250" "SFS_250_50"
+
+
+mkdir -p ~/www/stop/old/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_200/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_200_signal/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_225/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_225_signal/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_250/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_MT2_250_signal/
+
+mv ~/www/stop/varied_2017-06-13_juan_MT2_200/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_MT2_200_signal/ ~/www/stop/old/ 
+mv ~/www/stop/varied_2017-06-13_juan_MT2_225/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_MT2_225_signal/ ~/www/stop/old/ 
+mv ~/www/stop/varied_2017-06-13_juan_MT2_250/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_MT2_250_signal/ ~/www/stop/old/ 
+
+cp -r varied_2017-06-13_juan_MT2_200/        ~/www/stop/
+cp -r varied_2017-06-13_juan_MT2_200_signal/ ~/www/stop/
+cp -r varied_2017-06-13_juan_MT2_225/        ~/www/stop/
+cp -r varied_2017-06-13_juan_MT2_225_signal/ ~/www/stop/
+cp -r varied_2017-06-13_juan_MT2_250/        ~/www/stop/
+cp -r varied_2017-06-13_juan_MT2_250_signal/ ~/www/stop/
+
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_200/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_200_signal/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_225/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_225_signal/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_250/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_MT2_250_signal/
+
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_200/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_200_signal/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_225/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_225_signal/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_250/
+rm -r  ~/www/stop/old/varied_2017-06-13_juan_CutAndCount_250_signal/
+
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_200/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_200_signal/ ~/www/stop/old/ 
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_225/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_225_signal/ ~/www/stop/old/ 
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_250/        ~/www/stop/old/
+mv ~/www/stop/varied_2017-06-13_juan_CutAndCount_250_signal/ ~/www/stop/old/ 
+
+cp -r varied_2017-06-13_juan_CutAndCount_200/        ~/www/stop/
+cp -r varied_2017-06-13_juan_CutAndCount_200_signal/ ~/www/stop/
+cp -r varied_2017-06-13_juan_CutAndCount_225/        ~/www/stop/
+cp -r varied_2017-06-13_juan_CutAndCount_225_signal/ ~/www/stop/
+cp -r varied_2017-06-13_juan_CutAndCount_250/        ~/www/stop/
+cp -r varied_2017-06-13_juan_CutAndCount_250_signal/ ~/www/stop/
+
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_200/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_200_signal/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_225/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_225_signal/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_250/
+cp ~/www/stop/index.php ~/www/stop/varied_2017-06-13_juan_CutAndCount_250_signal/
 
 
 
