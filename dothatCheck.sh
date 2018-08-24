0fc9962b55768a76c3d8866802059ae340d1f664
Pietro Vischia <pietro.vischia@gmail.com>
2018-08-24 14:27:08 +0200

    Track checks

---
+++ dothatCheck.sh
@@ -0,0 +1,41 @@
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi Lumi --setPhysicsModelParameterRange Lumi=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"Lumi\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi ttV --setPhysicsModelParameterRange ttV=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"ttV\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi VV --setPhysicsModelParameterRange VV=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"VV\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi DY --setPhysicsModelParameterRange DY=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"DY\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi tW --setPhysicsModelParameterRange tW=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"tW\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi Nonprompt --setPhysicsModelParameterRange Nonprompt=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"Nonprompt\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi ttbar --setPhysicsModelParameterRange ttbar=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"ttbar\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi SFS_250_50 --setPhysicsModelParameterRange SFS_250_50=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"SFS_250_50\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi ue --setPhysicsModelParameterRange ue=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"ue\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi isr --setPhysicsModelParameterRange isr=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"isr\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi hdamp --setPhysicsModelParameterRange hdamp=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"hdamp\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi Scale --setPhysicsModelParameterRange Scale=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"Scale\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi JES --setPhysicsModelParameterRange JES=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"JES\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi Btag --setPhysicsModelParameterRange Btag=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"Btag\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi MisTag --setPhysicsModelParameterRange MisTag=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"MisTag\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi LepEff --setPhysicsModelParameterRange LepEff=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"LepEff\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi PU --setPhysicsModelParameterRange PU=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"PU\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi JER --setPhysicsModelParameterRange JER=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"JER\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi MET --setPhysicsModelParameterRange MET=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"MET\",501,-5,5\)
+combine -d 2017-06-28_NoMetCut/datacard_MT2_SFS_250_50_ElMu.root -M MultiDimFit --algo grid --poi TriggerEff --setPhysicsModelParameterRange TriggerEff=-3,3 --points=501 -t -1 --expectSignal 0
+ root -l -b drawNLL.C\(\"higgsCombineTest.MultiDimFit.mH120.root\",\"TriggerEff\",501,-5,5\)
+
