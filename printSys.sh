68045398971de1ace988fd0abe0787b3f1497d89
Pietro Vischia <pietro.vischia@gmail.com>
2018-08-24 14:25:39 +0200

    Reminder on printsys

---
+++ printSys.sh
@@ -0,0 +1,12 @@
+cd ../../../CMSSW_7_4_7/src/; cmsenv; cd - ; 
+#python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-27_xuan/datacard_MT2_SFS_200_50_ElMu.txt -a > ~/www/stop/2017-11-27_syst.html
+#python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-10-03_xuan/datacard_MT2_SFS_200_50_ElMu.txt -a > ~/www/stop/2017-10-03_syst.html
+#python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan/datacard_MT2_SFS_200_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_200_50_syst.html
+#python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan/datacard_MT2_SFS_225_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_225_50_syst.html
+#python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan/datacard_MT2_SFS_250_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_250_50_syst.html
+
+python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan_21bins/datacard_MT2_21_SFS_200_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_21bins_200_50_syst.html
+python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan_21bins/datacard_MT2_21_SFS_225_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_21bins_225_50_syst.html
+python /mnt_pool/ciencias_users/user/vischia/workarea/cmssw/combine/CMSSW_8_1_0/src/HiggsAnalysis/CombinedLimit/test/systematicsAnalyzer.py 2017-11-29bis_xuan_21bins/datacard_MT2_21_SFS_250_50_ElMu.txt -a > ~/www/stop/2017-11-29bis_21bins_250_50_syst.html
+
+cmsenv
